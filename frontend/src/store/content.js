966947d52457e2a185c95ff7bd2ff54086cb9541
night-dreamer <charon.deadpan@gmail.com>
2025-05-06 18:59:46 +0800

    fixed bug

--- frontend/src/store/content.js
+++ frontend/src/store/content.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useContentStore = create((set) => ({
-  contentType: JSON.parse(localStorage.getItem("contentType")),
+  contentType: JSON.parse(localStorage.getItem("contentType")) || "movie",
   setContentType: (type) => {
     localStorage.setItem("contentType", JSON.stringify(type));
     set({contentType: type})
